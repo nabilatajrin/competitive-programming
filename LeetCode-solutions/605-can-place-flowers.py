f2a4ddb0746135e91940b05bd479a6fd56b18f60
nabila <nabilatajrin@gmail.com>
2021-03-22 17:16:56 +0600

    05:17PM 22MAR2021

--- LeetCode-solutions/605-can-place-flowers.py
+++ LeetCode-solutions/605-can-place-flowers.py
@@ -32,8 +32,7 @@ class Solution():
         :rtype: bool
         """
         for i in range(len(flowerbed)):
-            if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and \
-                (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
+            if flowerbed[i] == 0 and (flowerbed[i-1] == 0) and (flowerbed[i+1] == 0):
                 flowerbed[i] = 1
                 n -= 1
             if n <= 0:
