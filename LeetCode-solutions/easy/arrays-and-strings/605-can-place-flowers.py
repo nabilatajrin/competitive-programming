35004cfdb3cf374be81b620bd349b5fd97675e5e
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-06-01 10:36:24 +0600

    Update 605-can-place-flowers.py

--- LeetCode-solutions/easy/arrays-and-strings/605-can-place-flowers.py
+++ LeetCode-solutions/easy/arrays-and-strings/605-can-place-flowers.py
@@ -1,5 +1,3 @@
-Easy
-
 You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
 Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, 
 return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
@@ -17,7 +15,6 @@ Constraints:
 flowerbed[i] is 0 or 1.
 There are no two adjacent flowers in flowerbed.
 0 <= n <= flowerbed.length
-
 
 ------------
 Solution 01:
@@ -39,15 +36,12 @@ class Solution:
                 return True
         return False
 
-
 -------------
 Solution 02:
-
 
 if not flowerbed[i]==0:
             i+=1
             continue
-
         elif i==0:
             if i+1 <=length:
                 if flowerbed[i+1]==0:
@@ -71,6 +65,5 @@ if not flowerbed[i]==0:
                 return True                
         else:
             i+=1 #go to the next possible spot. 
-        
+      
     return False
-
