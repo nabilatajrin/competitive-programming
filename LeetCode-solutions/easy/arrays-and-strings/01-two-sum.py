402b668ebe179d70871141a712a8416756d8a164
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-09-16 09:24:17 +0600

    Update 01-two-sum.py

--- LeetCode-solutions/easy/arrays-and-strings/01-two-sum.py
+++ LeetCode-solutions/easy/arrays-and-strings/01-two-sum.py
@@ -37,5 +37,22 @@ class Solution:
                 h[num] = i
             else:
                 return [h[n], i]
+            
+            
+#Solution 2
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                sum = nums[i] + nums[j]
+        
+                if sum == target:
+                    return[i, j]
 
 [ref: https://leetcode.com/problems/two-sum/]
