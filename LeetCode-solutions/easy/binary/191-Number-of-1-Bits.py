0bb80e2e16054949e25e2af87c0d96c55a38c463
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-04-15 12:59:01 +0600

    Create 191-Number-of-1-Bits.py

---
+++ LeetCode-solutions/easy/binary/191-Number-of-1-Bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        c = 0
+        while n:
+            n &= n - 1
+            c += 1
+        return c
