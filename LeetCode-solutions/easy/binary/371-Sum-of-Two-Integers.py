e62209092f9e76309c1be3acb5ae624699b6fced
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-04-19 11:36:58 +0600

    Create 371-Sum-of-Two-Integers.py

---
+++ LeetCode-solutions/easy/binary/371-Sum-of-Two-Integers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        list = [a, b]
+        return sum(list)
