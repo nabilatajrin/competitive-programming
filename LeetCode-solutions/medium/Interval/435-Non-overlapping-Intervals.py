4a21e5fca871b89085721137bab076f7b8852afb
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-05-06 19:46:55 +0600

    Update 435-Non-overlapping-Intervals.py

--- LeetCode-solutions/medium/Interval/435-Non-overlapping-Intervals.py
+++ LeetCode-solutions/medium/Interval/435-Non-overlapping-Intervals.py
@@ -11,3 +11,18 @@ class Solution:
                 curr = intervals[i]
                 
         return n - count   
+    
+    
+    #2nd solution
+    def eraseOverlapIntervals2(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        
+        res = 0
+        prevEnd = intervals[0][1]
+        for start, end in intervals[1:]:
+            if start >= prevEnd:
+                prevEnd = end
+            else:
+                res += 1
+                prevEnd = min(end, prevEnd)
+        return res
