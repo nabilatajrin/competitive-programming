5dd786a60cd7617832e5766efe21368d19c04b1a
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-06-01 22:16:58 +0600

    Create 213-House-Robber-II.py

---
+++ LeetCode-solutions/medium/dynamic-programming/213-House-Robber-II.py
@@ -0,0 +1,10 @@
+class Solution:
+    #Complexity: time complexity is O(n), because we use dp problem with complexity O(n) twice. Space complexity is O(1), because in python lists passed by reference and space complexity of House Robber problem is O(1).
+    def rob(self, nums):
+        def rob_helper(nums):
+            dp1, dp2 = 0, 0
+            for num in nums:
+                dp1, dp2 = dp2, max(dp1 + num, dp2)          
+            return dp2
+    
+        return max(nums[0] + rob_helper(nums[2:-1]), rob_helper(nums[1:]))
