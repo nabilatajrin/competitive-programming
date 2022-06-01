399e5a92161ce1ca801a64add76c801b14c39b08
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-06-01 22:15:04 +0600

    Create 198-House-Robber.py

---
+++ LeetCode-solutions/medium/dynamic-programming/198-House-Robber.py
@@ -0,0 +1,8 @@
+#Based on the recursive formula:
+
+class Solution:
+    def rob(self, nums):
+        last, now = 0, 0
+        for i in nums: last, now = now, max(last + i, now) 
+        return now
+        
