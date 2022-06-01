7929329ed27b251b547e9076f4c76e99263436c0
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-06-01 22:12:03 +0600

    Create 377-Combination-Sum-IV.py

---
+++ LeetCode-solutions/medium/dynamic-programming/377-Combination-Sum-IV.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combinationSum4(self, nums, target, memo=collections.defaultdict(int)):
+        if target < 0: return 0
+        if target not in memo:
+            memo[target] += sum((1, self.combinationSum4(nums, target - num))[target != num] 
+                                for num in nums)
+        return memo[target]
+    
+    #This is a recursive Python code
+    def combinationSum4WithLength(self, nums, target, length, memo=collections.defaultdict(int)):
+        if length <= 0: return 0
+        if length == 1: return 1 * (target in nums)
+        if (target, length) not in memo: 
+            for num in nums:
+                memo[target, length] += self.combinationSum4(nums, target - num, length - 1)
+        return memo[target, length]
