9ebd1634d8559de17a30552548a477d06fb81499
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-06-08 20:47:12 +0600

    Update 88-Merge-Sorted-Array.py

--- LeetCode-solutions/easy/arrays-and-strings/88-Merge-Sorted-Array.py
+++ LeetCode-solutions/easy/arrays-and-strings/88-Merge-Sorted-Array.py
@@ -3,7 +3,6 @@ Easy | Facebook|35,Microsoft|10,Amazon|8,Bloomberg|3,Walmart Labs|3,IBM|3,Oracle
 Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 The number of elements initialized in nums1 and nums2 are m and n respectively. You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
 
-
 Example 1:
 Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 Output: [1,2,2,3,5,6]
@@ -12,7 +11,6 @@ Example 2:
 Input: nums1 = [1], m = 1, nums2 = [], n = 0
 Output: [1]
  
-
 Constraints:
 
 nums1.length == m + n
@@ -20,12 +18,10 @@ nums2.length == n
 0 <= m, n <= 200
 1 <= m + n <= 200
 -109 <= nums1[i], nums2[i] <= 109
-
----------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------
 Solution:
 
 Approach 1 : Merge and sort
-
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
@@ -40,7 +36,6 @@ class Solution:
 
 
 Approach 2 : Three Pointers (Start From the Beginning)
-
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
@@ -66,7 +61,6 @@ class Solution:
 
 
 Approach 3 : Three Pointers (Start From the End)
-
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
