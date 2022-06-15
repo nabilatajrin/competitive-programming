d1298c897366886a794a14273bb509be45503330
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-06-15 20:12:15 +0600

    Update 238-Product-of-Array-Except-Self.py

--- LeetCode-solutions/easy/arrays-and-strings/238-Product-of-Array-Except-Self.py
+++ LeetCode-solutions/easy/arrays-and-strings/238-Product-of-Array-Except-Self.py
@@ -8,7 +8,7 @@ class Solution:
         n = len(nums)
         output = []
         for i in range(0,n):
-            output.append(p)
+            output.append(p) #The append() method in python adds a single item to the existing list.
             p = p * nums[i]
         p = 1
         for i in range(n-1,-1,-1):
