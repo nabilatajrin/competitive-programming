51441044160c375467624a1d08b02418425793e4
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-07-29 20:35:51 +0600

    Update 242-Valid-Anagram.py

--- LeetCode-solutions/easy/string/242-Valid-Anagram.py
+++ LeetCode-solutions/easy/string/242-Valid-Anagram.py
@@ -18,3 +18,17 @@ class Solution:
 
     def isAnagram3(self, s, t):
         return sorted(s) == sorted(t)
+    
+    
+Solution 2:
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
