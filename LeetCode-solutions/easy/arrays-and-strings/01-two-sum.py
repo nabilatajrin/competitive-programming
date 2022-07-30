d4f2d2b7ef77bcd92c1a114b7708797097182377
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-07-30 14:03:23 +0600

    Update 01-two-sum.py

--- LeetCode-solutions/easy/arrays-and-strings/01-two-sum.py
+++ LeetCode-solutions/easy/arrays-and-strings/01-two-sum.py
@@ -56,3 +56,15 @@ class Solution:
                     return[i, j]
 
 [ref: https://leetcode.com/problems/two-sum/]
+
+
+Solution 3:
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
