30d9bf7b115d48d8c1a9a761269b86905b49ef82
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-12-18 11:24:51 +0600

    Update 100-Same-Tree.py

--- LeetCode-solutions/easy/tree/100-Same-Tree.py
+++ LeetCode-solutions/easy/tree/100-Same-Tree.py
@@ -11,11 +11,13 @@ class Solution:
         if p and q:
             return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         return p is q
+    
     #The "tupleify" way:
     def isSameTree(self, p, q):
         def t(n):
             return n and (n.val, t(n.left), t(n.right))
         return t(p) == t(q)
+    
     #The first way as one-liner:
     def isSameTree(self, p, q):
         return p and q and p.val == q.val and all(map(self.isSameTree, (p.left, p.right), (q.left, q.right))) or p is q
