f059a9325b1f6e7883f2b88644f1c9fa4fe3fd1b
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-12-20 11:19:02 +0600

    Update 121-best-time-to-buy-and-sell-stock.py

--- LeetCode-solutions/easy/arrays-and-strings/121-best-time-to-buy-and-sell-stock.py
+++ LeetCode-solutions/easy/arrays-and-strings/121-best-time-to-buy-and-sell-stock.py
@@ -14,7 +14,8 @@ class Solution:
             right += 1
         return max_profit
 ------------------------------------------------------------------
-Your input
+
+Input
 [7,1,5,3,6,4]
 Output
 5
