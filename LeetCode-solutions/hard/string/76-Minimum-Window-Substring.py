1b9e4af76404eb7852c84ae2108a66c66fc7cd25
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-01-30 12:24:50 +0600

    Update 76-Minimum-Window-Substring.py

--- LeetCode-solutions/hard/string/76-Minimum-Window-Substring.py
+++ LeetCode-solutions/hard/string/76-Minimum-Window-Substring.py
@@ -29,27 +29,20 @@ class Solution:
         
         if not s or not t or len(s) < len(t):
             return ''
-        
         t_counter = Counter(t)
         chars = len(t_counter.keys())
-        
         s_counter = Counter()
         matches = 0
-        
         answer = ''
-        
         i = 0
         j = -1 # make j = -1 to start, so we can move it forward and put s[0] in s_counter in the extend phase 
         
         while i < len(s):
-            
             # extend
-            if matches < chars:
-                
+            if matches < chars: 
                 # since we don't have enough matches and j is at the end of the string, we have no way to increase matches
                 if j == len(s) - 1:
                     return answer
-                
                 j += 1
                 s_counter[s[j]] += 1
                 if t_counter[s[j]] > 0 and s_counter[s[j]] == t_counter[s[j]]:
@@ -68,5 +61,4 @@ class Solution:
                     answer = s[i:j+1]
                 elif (j - i + 1) < len(answer):
                     answer = s[i:j+1]
-        
         return answer
