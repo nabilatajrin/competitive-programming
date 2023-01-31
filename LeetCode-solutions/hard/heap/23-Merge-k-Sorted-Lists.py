968c838d23f670d0e8baddad69b264103941810c
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-01-31 22:58:25 +0600

    Update 23-Merge-k-Sorted-Lists.py

--- LeetCode-solutions/hard/heap/23-Merge-k-Sorted-Lists.py
+++ LeetCode-solutions/hard/heap/23-Merge-k-Sorted-Lists.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         ListNode.__eq__ = lambda self, other: self.val == other.val
@@ -12,14 +13,12 @@ class Solution:
         for i in lists:
             if i:
                 heapq.heappush(h, (i.val, i))
-
         while h:
             node = heapq.heappop(h)[1]
             tail.next = node
             tail = tail.next
             if node.next:
                 heapq.heappush(h, (node.next.val, node.next))
-
         return head.next
     
     
@@ -29,7 +28,6 @@ class Solution:
         head = tail = ListNode(0)
         for i in range(len(lists)):
             heapq.heappush(h, (lists[i].val, i, lists[i]))
-
         while h:
             node = heapq.heappop(h)
             node = node[2]
@@ -38,6 +36,5 @@ class Solution:
             if node.next:
                 i+=1
                 heapq.heappush(h, (node.next.val, i, node.next))
-
         return head.next
         
