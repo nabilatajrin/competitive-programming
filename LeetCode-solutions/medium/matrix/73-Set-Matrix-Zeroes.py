5151fab6c85de3eed2c83caca70a21e120686440
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-02-03 15:37:24 +0600

    Update 73-Set-Matrix-Zeroes.py

--- LeetCode-solutions/medium/matrix/73-Set-Matrix-Zeroes.py
+++ LeetCode-solutions/medium/matrix/73-Set-Matrix-Zeroes.py
@@ -1,13 +1,12 @@
 class Solution:
+    #solution 1: More optimized using O(m + n) space:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        #solution 1: More optimized using O(m + n) space:
         # input validation
         if not matrix:
             return []
-
         m = len(matrix)
         n = len(matrix[0])
 
@@ -18,18 +17,17 @@ class Solution:
              if matrix[row][col] == 0:
                  zeroes_row[row] = True
                  zeroes_col[col] = True
-
         for row in range(m):
          for col in range(n):
              if zeroes_row[row] or zeroes_col[col]:
                  matrix[row][col] = 0
                 
+		
                 
     #solution 2: Most optimized using O(1) space:
     def setZeroes_2(self, matrix: List[List[int]]) -> None:
         m = len(matrix)
         n = len(matrix[0])
-		
         first_row_has_zero = False
         first_col_has_zero = False
         
@@ -52,7 +50,6 @@ class Solution:
         if first_row_has_zero:
             for col in range(n):
                 matrix[0][col] = 0
-        
         if first_col_has_zero:
             for row in range(m):
                 matrix[row][0] = 0
