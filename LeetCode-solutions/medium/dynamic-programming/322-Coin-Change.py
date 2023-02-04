ffa7ef409e77f8be94fb95309970763e012855ec
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-02-04 12:08:46 +0600

    Update 322-Coin-Change.py

--- LeetCode-solutions/medium/dynamic-programming/322-Coin-Change.py
+++ LeetCode-solutions/medium/dynamic-programming/322-Coin-Change.py
@@ -5,7 +5,6 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         MAX = float('inf')
         dp = [0] + [MAX] * amount
-
         for i in range(1, amount + 1):
             dp[i] = min([dp[i - c] if i - c >= 0 else MAX for c in coins]) + 1
 
