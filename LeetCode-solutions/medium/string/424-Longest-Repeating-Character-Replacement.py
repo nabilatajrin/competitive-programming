3ce4db640e952145d6e5c49f140fcc5c59815585
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-02-10 10:11:25 +0600

    Update 424-Longest-Repeating-Character-Replacement.py

--- LeetCode-solutions/medium/string/424-Longest-Repeating-Character-Replacement.py
+++ LeetCode-solutions/medium/string/424-Longest-Repeating-Character-Replacement.py
@@ -1,9 +1,11 @@
 class Solution:
     #Simple Python two-pointer solution
+    
     def characterReplacement(self, s, k):        
         longest_window = 0
         window_counts = collections.defaultdict(int)
         l = 0
+        
         for r in range(len(s)):
             window_counts[s[r]] += 1
             while r - l + 1 - max(window_counts.values()) > k:
