3cb51448f41e76e2d9870e14b04cf83b256a80b9
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-03-04 18:29:12 +0600

    Update 217-Contains-Duplicate.py

--- LeetCode-solutions/easy/arrays-and-strings/217-Contains-Duplicate.py
+++ LeetCode-solutions/easy/arrays-and-strings/217-Contains-Duplicate.py
@@ -16,7 +16,6 @@ Solution 2:
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hashset = set()
-
         for n in nums:
             if n in hashset:
                 return True
