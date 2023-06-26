bd1ab87d6acab0059a840afbe540569d937ccc00
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-06-26 09:45:10 +0600

    Update 33-search-in-rotated-sorted-array.py

--- LeetCode-solutions/medium/arrays-and-strings/33-search-in-rotated-sorted-array.py
+++ LeetCode-solutions/medium/arrays-and-strings/33-search-in-rotated-sorted-array.py
@@ -17,17 +17,16 @@ class Solution:
             No rotated:
             1 2 3 4 5 6 7
                  mid
-                 
             left rotated: pivot at the left side of the origin sorted array, A[mid] >= A[left]
             3 4 5 6 7 1 2
                  mid
             search in A[left] ~ A [mid] if A[left] <= target < A[mid] else, search right side
-            
             right rotated: pivot at the right side of the origin sorted array, A[mid] < A[left]
             6 7 1 2 3 4 5
                  mid          
             search in A[mid] ~ A[right] if A[mid] < target <= A[right] else, search left side
             """
+            
             if nums[mid] >= nums[left]: # left rotated
                 # in ascending order side
                 if nums[left] <= target and target < nums[mid]:
