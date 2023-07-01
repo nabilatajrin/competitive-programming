4ce135651c437bc68dfc4468310f0314863c114b
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-07-01 08:00:14 +0600

    Update 198-House-Robber.py

--- LeetCode-solutions/medium/dynamic-programming/198-House-Robber.py
+++ LeetCode-solutions/medium/dynamic-programming/198-House-Robber.py
@@ -1,8 +1,6 @@
 #Based on the recursive formula:
-
 class Solution:
     def rob(self, nums):
         last, now = 0, 0
         for i in nums: last, now = now, max(last + i, now) 
         return now
-        
