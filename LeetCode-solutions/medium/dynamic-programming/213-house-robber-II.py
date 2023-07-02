dadfbf4b266d263e416f54c1ba8e1bf610893291
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-07-02 10:05:25 +0600

    Update 213-house-robber-II.py

--- LeetCode-solutions/medium/dynamic-programming/213-house-robber-II.py
+++ LeetCode-solutions/medium/dynamic-programming/213-house-robber-II.py
@@ -6,5 +6,4 @@ class Solution:
             for num in nums:
                 dp1, dp2 = dp2, max(dp1 + num, dp2)          
             return dp2
-    
         return max(nums[0] + rob_helper(nums[2:-1]), rob_helper(nums[1:]))
