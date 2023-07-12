912271466eb4172d69eee41a5c68b050eb5cc66d
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-07-12 10:25:44 +0600

    Update and rename 133-Clone-Graph.py to 133-clone-graph.py

--- LeetCode-solutions/medium/graph/133-clone-graph.py
+++ LeetCode-solutions/medium/graph/133-clone-graph.py
@@ -9,21 +9,15 @@ class Node:
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node: return node
-        
         q, clones = deque([node]), {node.val: Node(node.val, [])}
         while q:
             cur = q.popleft() 
             cur_clone = clones[cur.val]            
-
             for ngbr in cur.neighbors:
                 if ngbr.val not in clones:
                     clones[ngbr.val] = Node(ngbr.val, [])
                     q.append(ngbr)
-                    
                 cur_clone.neighbors.append(clones[ngbr.val])
-                
         return clones[node.val]
-    
     #Time: O(V + E) - for BFS
     #Space: O(V) - for the hashmap
-        
