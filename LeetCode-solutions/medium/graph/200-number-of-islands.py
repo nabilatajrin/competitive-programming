95fd26a58e2e3e0785c17cdcc3c2d1e812aaf470
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-07-13 12:16:14 +0600

    Update and rename 200-Number-of-Islands.py to 200-number-of-islands.py

--- LeetCode-solutions/medium/graph/200-number-of-islands.py
+++ LeetCode-solutions/medium/graph/200-number-of-islands.py
@@ -3,7 +3,6 @@ class Solution:
     def numIslands(self, grid):
         if not grid:
             return 0
-
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
@@ -11,7 +10,6 @@ class Solution:
                     self.dfs(grid, i, j)
                     count += 1
         return count
-
     def dfs(self, grid, i, j):
         if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
             return
