349aa6ff09d9beb67d13b448224b4d0f02a5de48
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-09-21 14:11:39 +0600

    Update 53-maximum-subarray.py

--- LeetCode-solutions/easy/arrays-and-strings/53-maximum-subarray.py
+++ LeetCode-solutions/easy/arrays-and-strings/53-maximum-subarray.py
@@ -5,3 +5,17 @@ class Solution:
         for i in range(1,len(nums)):
             nums[i] = max(nums[i], nums[i-1] + nums[i])
         return max(nums)
+-----------------------
+Here's a Python solution to solve this problem using Kadane's algorithm:
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = nums[0]  # Initialize max_sum with the first element of the array
+        current_sum = nums[0]  # Initialize current_sum with the first element
+
+        for i in range(1, len(nums)):
+            # Update the current_sum to either the current element or the sum of the current element and the previous current_sum
+            current_sum = max(nums[i], current_sum + nums[i])
+            # Update max_sum if the current_sum is greater
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum
