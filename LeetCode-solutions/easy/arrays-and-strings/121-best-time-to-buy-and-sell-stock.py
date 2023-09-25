b71b6662cf99374d40e69ab4e007fd68ede10744
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-09-25 14:15:06 +0600

    Update 121-best-time-to-buy-and-sell-stock.py

--- LeetCode-solutions/easy/arrays-and-strings/121-best-time-to-buy-and-sell-stock.py
+++ LeetCode-solutions/easy/arrays-and-strings/121-best-time-to-buy-and-sell-stock.py
@@ -10,41 +10,36 @@ def maxProfit(self, prices):
         return max_profit
 -------------------------------------
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
-        min_price = float('inf')  # Set the initial minimum price to positive infinity.
-
+    def maxProfit(prices):
+        if not prices:
+            return 0  # If there are no prices, there's no profit to be made
+    
+        min_price = prices[0]  # Initialize the minimum price to the first day's price
+        max_profit = 0  # Initialize the maximum profit to 0
+    
         for price in prices:
-            # Imagine you are riding a roller coaster!
-            if price < min_price:
-                # Woohoo! We found a new dip in the roller coaster. Let's update the minimum price.
-                min_price = price
-            elif price - min_price > max_profit:
-                # Whee! We found a peak after the dip, and it gives us a higher profit!
-                max_profit = price - min_price
-
+            # Update the minimum price if we find a lower price
+            min_price = min(min_price, price)
+            
+            # Calculate the profit we would make by selling at the current price
+            current_profit = price - min_price
+            
+            # Update the maximum profit if the current profit is higher
+            max_profit = max(max_profit, current_profit)
+    
         return max_profit
-------------------------------
-
-Input:
-[7,1,5,3,6,4]
-
-Output:
-5
-Expected
-5
-------------------------------
-
+        -------------------
 Explanation:
-1. The function maxProfit takes a list prices as input, representing the stock prices on different days.
-2. It initializes maxProfit variable to 0, which will keep track of the maximum profit we can achieve.
-3. It sets min_price to positive infinity (float('inf')) as an initial placeholder for the minimum price. 
-     We can think of this as the lowest point of the roller coaster ride.
-4. The function then goes through each price in the prices list.
-5. It imagines that you are riding a roller coaster. When the price goes down (you encounter a dip), 
-it gets excited and updates the min_price to the current price.
-6. When the price goes up (you reach a peak after a dip), it checks if the difference between the current 
-price and the min_price gives a higher profit than the current max_profit. If it does, the function 
-updates max_profit with the new higher value.
-7. After looping through all the prices, the function returns the max_profit, which represents the maximum 
-profit you can achieve by buying at the lowest point (dip) and selling at the highest point (peak) in the roller coaster ride of stock prices.
+
+We start by initializing min_price to the price on the first day and max_profit to 0.
+We then iterate through the prices array, starting from the second day (index 1). For each day's price:
+We update min_price if we find a lower price. This ensures that min_price always represents the lowest price seen so far.
+We calculate the profit we would make by selling at the current price, which is price - min_price. This represents the profit 
+                                                               if we buy at min_price and sell at the current price.
+We update max_profit to be the maximum of the current max_profit and current_profit. This step ensures that max_profit always 
+                                                               contains the maximum profit seen so far.
+After iterating through all prices, max_profit will contain the maximum profit that can be achieved by buying and selling the 
+                       stock once.
+We return max_profit as the final result.
+This solution has a time complexity of O(n) because we iterate through the prices array once, where n is the number of days. It efficiently finds the maximum profit by keeping track of the minimum price and the maximum profit seen so far during the iteration.
+
