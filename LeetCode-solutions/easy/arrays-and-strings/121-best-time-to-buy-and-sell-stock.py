4aa94ba66830ab38d66a669e59d6b57b7744998b
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-09-25 13:07:15 +0600

    Update 121-best-time-to-buy-and-sell-stock.py

--- LeetCode-solutions/easy/arrays-and-strings/121-best-time-to-buy-and-sell-stock.py
+++ LeetCode-solutions/easy/arrays-and-strings/121-best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,15 @@
 #LeetCode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-
+def maxProfit(self, prices):
+    max_profit = 0
+        min_price = prices[0]
+    
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+    
+        return max_profit
+-------------------------------------
 class Solution:
-    class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_profit = 0
         min_price = float('inf')  # Set the initial minimum price to positive infinity.
